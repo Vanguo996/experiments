080c3d650e413014c5c8e6b4b2b4f48306cc631b
vanguo996 <guowang939@gmail.com>
2021-10-05 16:26:36 +0800

    build centos-visa image

---
+++ test.py
@@ -0,0 +1,21 @@
+import pyvisa as visa
+
+def connection_test():
+        
+    rm = visa.ResourceManager()
+    # instr = rm.open_resource("visa://192.168.1.108/GPIB0::7::INSTR")
+
+    test_name = "visa://192.168.1.108/GPIB0::INTFC"
+    instr = rm.open_resource(test_name)
+    # ok = instr.query("*idn?")
+    print(instr)
+
+
+def visalib_test():
+
+    rm = visa.ResourceManager("@ivi")
+    print(rm)
+
+connection_test()
+
+# visalib_test()
